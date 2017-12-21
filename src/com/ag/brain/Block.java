7017335bb9cda17a20bbf909e19016fa96e112e9
Alessandro Guaresti <alessguar@gmail.com>
2017-12-21 08:44:37 -0800

    the sructure is there just dont run it

--- src/com/ag/brain/Block.java
+++ src/com/ag/brain/Block.java
@@ -1,12 +1,17 @@
 package com.ag.brain;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 public class Block{
 
 	private Date date;
 	private int index;
+	private int nonce;
 	private String data;
+	private String nuHash;
 	private String prevHash;
 
 
@@ -15,6 +20,7 @@ public class Block{
 		date = new Date();
 		this.data = data;
 		prevHash = "";
+		nuHash = createNewHash(prevHash);
 	}
 
 	public Block(String data, int ind, String preHash){//blocks after genesis
@@ -22,7 +28,40 @@ public class Block{
 		this.data = data;
 		index = ind;
 		prevHash = preHash;
+		nuHash = createNewHash(prevHash);
 	}
+
+	public String createNewHash(String preHash){
+		try{
+			MessageDigest digst = MessageDigest.getInstance("SHA-256");
+			String s = getData() + getDate() + String.valueOf(getIndex()) + preHash;
+			byte[] hashbytes = digst.digest(s.getBytes("UTF-8"));
+			StringBuffer sBuff = new StringBuffer();
+			for (int i = 0;i<hashbytes.length; i++) {
+				sBuff.append(Integer.toString((hashbytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			String newHash = sBuff.toString();
+			while(!isValidHash(newHash)){
+				nonce++;
+				createNewHash(newHash);
+			}
+			return newHash;
+		}catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			System.out.println(e);
+		}
+		return "";
+	}
+
+	public boolean isValidHash(String pHash){
+		char[] c = {pHash.charAt(0),pHash.charAt(1)/*,pHash.charAt(2),pHash.charAt(3)*/};
+		for(char i : c){
+			if (i != '0') {
+				return false;
+			}
+		}
+		return true;
+	}
+
 
 	//accessors
 	public String getDate(){
@@ -33,13 +72,20 @@ public class Block{
 		return index;
 	}
 
+	public int getNonce(){
+		return nonce;
+	}
+
 	public String getData(){
 		return data;
+	}
+
+	public String getNewHash(){
+		return nuHash;
 	}
 
 	public String getPreviousHash(){
 		return prevHash;
 	}
-
 
 }
--- src/com/ag/main/Main.java
+++ src/com/ag/main/Main.java
@@ -3,9 +3,23 @@ package com.ag.main;
 import com.ag.brain.*;
 
 public class Main {
+
+	public static Block gen;
+	public static Block b1;
+	public static Block b2;
+	public static Block b3;
 	
 	public static void main(String [] args){
-
+		BlockChain b = new BlockChain();
+		gen = new Block("Hey you, I'm a Computer! Stop all your downloading!");
+		b1 = new Block("knock knock.", 01, gen.getNewHash());		
+		// b2 = new Block("Hi I am data", 02, b1.getNewHash());
+		// b3 = new Block("Hi data im block", 03, b2.getNewHash());
+		b.addNewBlock(gen);
+		b.addNewBlock(b1);
+		// b.addNewBlock(b2);
+		// b.addNewBlock(b3);
+		System.out.println(b);
 	}
 
 }
